59b96771d77db3acb1382db02e28c415d4b5c1fd
Janakyraam Y <janakyraam1996@gmail.com>
2023-10-04 22:27:09 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+//console.warn("Hello World");
+//console.log("Hello World");
+// var a=20;
+// let b=30;
+// const c=40;
+// c=30;
+// console.log(a+b+c);
+// var a=20;
+// if(a==20){
+//     console.log("matched");
+// }
+// for(i=0; i<10; i++){
+//     console.log(i)
+// }
+//import function and variables from one file to the another file
+// const app=require('./app')
+// let arr=[4,1,6,3,4,89,0];
+//console.log(arr);
+//console.log(arr[0]);
+//console.log(app);
+//console.log(app.xyz());
+// const result=arr.filter((item)=>{
+    //console.log(item)
+    //return item===4
+    // return item>4
+//     return item>=4
+// });
+// console.warn(result);
+
+// const fs=require('fs').writeFileSync;
+// console.log("Code Step By Step");
+// fs("hello.txt","Code Step By Step");
+// fs.writeFileSync("abc.txt","Code Step By Step");
+// console.log(__dirname);
+// console.log(__filename);
+
+const http = require('http');
+
+// function dataControl(req,res){
+//     res.write("<h1>Code Step By Step</h1>");
+//     res.end();
+// }
+
+// const dataControl=(req,res)=>{
+//     res.write("<h1>Code Step By Step</h1>");
+//     res.end();
+// }
+
+// http.createServer((req,res)=>{
+//     res.write("<h1>Hello World</h1>");
+//     res.end();
+// }).listen(4500); //function chaining in javascript
+
+// http.createServer(dataControl).listen(4500);
+
+console.log("package.json");
